8eb0cc9da85e5b375e96b95fc2b55816e53b7ec7
unknown <anjumarnab050@gmail.com>
2025-06-11 00:11:14 +0600

    I:

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:img_picker/main.dart';
+import 'package:pixelize/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
