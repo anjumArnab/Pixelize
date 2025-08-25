6d11b126ae09f86fd6b9babe7c6f43aa9c857e29
unknown <anjumarnab050@gmail.com>
2025-08-25 14:07:50 +0600

    final VoidCallback onTap

--- lib/widgets/add_image_slot.dart
+++ lib/widgets/add_image_slot.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 
 class AddImageSlot extends StatelessWidget {
   final VoidCallback onTap;
+  final bool showBorder;
+  final String? tooltip;
 
   const AddImageSlot({
     Key? key,
     required this.onTap,
+    this.showBorder = true,
+    this.tooltip,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    Widget child = GestureDetector(
       onTap: onTap,
       child: Container(
         width: 50,
@@ -18,10 +22,20 @@ class AddImageSlot extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(12),
-          border: Border.all(
-            color: Colors.grey[300]!,
-            style: BorderStyle.solid,
-          ),
+          border: showBorder
+              ? Border.all(
+                  color: Colors.grey[300]!,
+                  style: BorderStyle.solid,
+                )
+              : null,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 1),
+            ),
+          ],
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -43,5 +57,15 @@ class AddImageSlot extends StatelessWidget {
         ),
       ),
     );
+
+    // Wrap with tooltip if provided
+    if (tooltip != null) {
+      child = Tooltip(
+        message: tooltip!,
+        child: child,
+      );
+    }
+
+    return child;
   }
 }
