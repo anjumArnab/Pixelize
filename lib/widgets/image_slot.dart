8214cb8a774d0e08bb0c9c554140f230a948dc24
unknown <anjumarnab050@gmail.com>
2025-08-26 10:18:16 +0600

    XFile?

--- lib/widgets/image_slot.dart
+++ lib/widgets/image_slot.dart
@@ -1,44 +1,92 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageSlot extends StatelessWidget {
   final bool hasImage;
+  final XFile? imageFile;
+  final VoidCallback? onLongPress;
+  final VoidCallback? onTap;
 
   const ImageSlot({
     super.key,
     required this.hasImage,
+    this.imageFile,
+    this.onLongPress,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 50,
-      height: 50,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(color: Colors.grey[200]!),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.1),
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: const Offset(0, 1),
-          ),
-        ],
-      ),
-      child: hasImage
-          ? Container(
-              decoration: BoxDecoration(
+    return GestureDetector(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.grey[200]!),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.1),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 1),
+            ),
+          ],
+        ),
+        child: hasImage && imageFile != null
+            ? ClipRRect(
                 borderRadius: BorderRadius.circular(12),
-                color: Colors.grey[200],
+                child: SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: kIsWeb
+                      ? Image.network(
+                          imageFile!.path,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Container(
+                              color: Colors.grey[200],
+                              child: Icon(
+                                Icons.image,
+                                color: Colors.grey[400],
+                                size: 30,
+                              ),
+                            );
+                          },
+                        )
+                      : Image.file(
+                          File(imageFile!.path),
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) {
+                            return Container(
+                              color: Colors.grey[200],
+                              child: Icon(
+                                Icons.image,
+                                color: Colors.grey[400],
+                                size: 30,
+                              ),
+                            );
+                          },
+                        ),
+                ),
+              )
+            : Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: Colors.grey[200],
+                ),
+                child: Icon(
+                  Icons.image,
+                  color: Colors.grey[400],
+                  size: 30,
+                ),
               ),
-              child: Icon(
-                Icons.image,
-                color: Colors.grey[400],
-                size: 30,
-              ),
-            )
-          : const SizedBox(),
+      ),
     );
   }
 }
